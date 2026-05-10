00bac6d1459f8a4397d6ef612661afb052d39df7
LaGooseDev <thegooseempire@gmail.com>
2026-05-09 20:25:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+const All = {
+    "uk": "unitedkingdom",
+    "czechia": "czechrepublic",
+    "car": "centralafricanrepublic",
+};
+
+let Countries = [];
+let Guessed = {};
+
+fetch("Countries.json")
+.then(Response => Response.json())
+.then(Data => {
+    Countries = Data;
+
+    UpdateProgress();
+});
+
+function IsACountry(Text) {
+    let From = null;
+
+    if (Countries.includes(Text)) {
+        From = Text;
+    } else {
+        From = All[Text];
+    }
+
+    return From;
+}
+
+function GuessedCountry(Name) {
+    const Message = document.getElementById("Message");
+
+    if (!(Name in Guessed)) {
+        Guessed[Name] = true;
+
+        Message.innerText = `Correct! (${Object.keys(Guessed).length}/${Countries.length})`;
+        Message.style.color = "lime";
+    } else {
+        Message.innerText = "Already guessed.";
+        Message.style.color = "yellow";
+    }
+
+    UpdateProgress();
+}
+
+function Input(Text) {
+    const Filtered = [...Text]
+        .filter(Char => /[a-zA-Z]/.test(Char))
+        .join("");
+
+    const Final = Filtered.toLowerCase();
+
+    const Country = IsACountry(Final);
+
+    const Message = document.getElementById("Message");
+
+    if (Country) {
+        GuessedCountry(Final);
+    } else {
+        Message.innerText = "Wrong.";
+        Message.style.color = "red";
+    }
+}
+
+function WaitForValue() {
+    const InputBox = document.getElementById("Input");
+
+    Input(InputBox.value);
+
+    InputBox.value = "";
+}
+
+function UpdateProgress() {
+    document.getElementById("Progress").innerText =
+        `${Object.keys(Guessed).length} / ${Countries.length}`;
+}
